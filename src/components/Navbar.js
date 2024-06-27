d08b054e588020834519cad445af4299d9e66143
Juanpa Quesada <jpqcaballero@gmail.com>
2024-06-27 15:21:28 -0600

    navbar normal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,16 @@ import '../styles/Navbar.css';
 import logo from '../imgs/logo-blanco.PNG';
 
 const Navbar = () => {
-  const scrollToSection = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
     <nav className="navbar">
       <ul className="left">
-        <li><a onClick={() => scrollToSection('portfolio')} className='portfolio-button'>Portfolio</a></li>
-        <li><a onClick={() => scrollToSection('albums')} className='album-button'>Albums</a></li>
+        <li><a href="#portfolio" className='portfolio-button'>Portfolio</a></li>
+        <li><a href="#albums" className='album-button'>Albums</a></li>
       </ul>
-      <img src={logo} alt='Juanpa Quesada Photography Logo' className='navbar-logo' onClick={() => scrollToSection('welcome')} />
+      <img src={logo} alt='Juanpa Quesada Photography Logo' className='navbar-logo' href="#welcome" />
       <ul className="right">
-        <li><a onClick={() => scrollToSection('about')} className='about-button'>About</a></li>
-        <li><a onClick={() => scrollToSection('contact')} className='contact-button'>Contact</a></li>
+        <li><a href="#about" className='about-button'>About</a></li>
+        <li><a href="#contact" className='contact-button'>Contact</a></li>
       </ul>
     </nav>
   );
